68315076033d6f0e26b0fd88ad60caebb30320ca
Samuel Ejalonibu <90863438+SamuelWorld20@users.noreply.github.com>
2023-12-06 04:06:24 +0100

    Create Word_Blanks.js

---
+++ Basic_JavaScript/Word_Blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " found me holding a very " + myAdjective + " bone, it " + myVerb + " very " + myAdverb + " to get it"; // Change this line
+// Only change code above this line
