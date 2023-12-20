a330bc5e7e4dce419d616d0f5d299f31a37b8809
Samuel Ejalonibu <90863438+SamuelWorld20@users.noreply.github.com>
2023-12-21 00:03:01 +0100

    Create Manipulating_Complex_Objects.js

---
+++ Basic_JavaScript/Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Record here
+  {
+    artist: "Sammy",
+      title: "ALX SE",
+      release_year: 2024,
+      formats: [
+        "CD",
+        "8T",
+        "LP"
+      ],
+  }
+];
